25c5208ea4efeca28c34f37700e3b7bd03504676
Vadym Malyshev <malysheff@ukr.net>
2019-02-27 13:23:13 +0100

    hasbin fix

--- cmd/hasbin/hasbin.go
+++ cmd/hasbin/hasbin.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"git.tor.ph/hiveon/pool/config"
 	"git.tor.ph/hiveon/pool/internal/casbin"
 	"github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -13,7 +18,16 @@ func main() {
 
 	if err != nil {
 		logrus.Panicf("can't start synchronizer: %s", err)
+		errs <- err
 	}
 
 	sync.Start(errs)
+	log.Info("Started syncronizer ")
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	log.Info("terminated ", <-errs)
 }
